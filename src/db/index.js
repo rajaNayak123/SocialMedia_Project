b70e461a2f19e8519b86ed819e846a6b229e1495
rajaNayak123 <nayakraja151@gmail.com>
2024-07-07 11:20:38 +0530

    define the absolute path

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 import { DB_NAME } from '../constants.js'
-
- const connectDB = async () => {
+const connectDB = async () => {
     try {
-       const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-    //    console.log(`\n MonngoDB connected !! DB Host: ${connectionInstance.connection.host}`);
+        // console.log(process.env.MONGODB_URL)
+         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
     } catch (error) {
         console.error(error);
         process.exit(1);
     }
 }
 
-export default connectDB ;
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 // require('dotenv').config({path:'./env'})
 
-import dotenv from 'dotenv'
 import connectDB from './db/index.js';
 import {app} from './app.js';
 
-dotenv.config({ path: './env' })
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import dotenv from "dotenv"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const envFilePath = join(__dirname, "../.env")
+dotenv.config({path: envFilePath})
 
 
 connectDB().then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log("server started on port " + `${process.env.PORT}`)
+        console.log(`server started on port ${process.env.PORT}`)
     })
 }).catch((err) => {
     console.log("server connection error: " + err);
